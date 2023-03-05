f062da1c633b70c463d3e22e7398e2190feef68d
alessandra <alessandra@Hiro.local>
2023-03-05 23:33:26 +0000

    ale committing beginnign of work

---
+++ Scripts/Intro.R
@@ -0,0 +1,8 @@
+#OWNER: Alessandra Cianfanelli 
+#EMAIL: alessandra.cianfanelli@gmail.com
+#DESC: This is my first script in my, perhaps futile, 
+  #attemp at creating a dashboard for the 2023 F1 season 
+
+library("shiny")
+library("rsconnect")  # For publishing apps online
+library("agridat")  # For the dataset in today's tutorial
